a1c571e91be4de90b43f869024fd179b46c05818
JoaoPedroBrandGuimaraes <joaopbg1@al.insper.edu.br>
2025-04-15 22:00:30 +0000

    .

--- Posicionamento e captura de posição.cpp
+++ Posicionamento e captura de posição.cpp
@@ -1,5 +1,30 @@
 #include "mbed.h"
-void salvarPosicaoColeta() {
-    posicao_coleta_X = posicao_X;
-    posicao_coleta_Y = posicao_Y;
-    posicao_coleta_Z = posicao_Z;
+void SalvaPosicaoCOLETA() {
+    //armazenando as posicoes atuais dos eixos X,Y e Z
+    posicao_coletaX = posicao_X;
+    posicao_coletaY = posicao_Y;
+    posicao_coletaZ = posicao_Z;
+
+    //indica que a posicao atual foi salva
+    posicao_de_coleta_salva = true;
+
+    //limpa o display LCD e mensagem dizendo que foi salva a posicao atual
+    lcd.cls();
+    lcd.locate(0,0);
+    lcd.printf("coleta salva:");
+
+    //msg do eixo X
+    lcd.locate(0,1);
+    lcd.print("X = %d",posicao_coletaX);
+    //msg do eixo Y
+    lcd.locate(0,2);
+    lcd.print("Y = %d",posicao_coletaY);
+    //msg do eixo Z
+    lcd.locate(0,3);
+    lcd.print("Z = %d",posicao_coletaZ);
+
+    wait_ms(5000);
+    lcd.cls();
+}
+
+void salvaposicaoLIBERAÇÃO
